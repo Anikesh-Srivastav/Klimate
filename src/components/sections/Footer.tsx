8934499281ddeca00507cc7023d99686177ca43b
Anikesh-Srivastav <anikeshsrivastab@gmail.com>
2025-06-24 22:12:18 +0530

    final changes

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
             GitHub
           </a>
           <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=anikeshsrivastab@gmail.com.com"
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=anikeshsrivastab@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:underline hover:text-primary transition"
